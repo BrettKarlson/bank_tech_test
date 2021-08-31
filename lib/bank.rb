eeda3dac24f0a6930be306bfea9169f39edbbb4e
Brett Karlson <brettjkarlson@gmail.com>
2021-08-31 17:09:34 +0100

    returns balance and deposit

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,6 +1,13 @@
 class Bank
 
-  def balance
-    0    
+  attr_reader :balance
+
+  def initialize(balance = 0)
+    @balance = balance 
   end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Bank do
   it 'shows the initial balance' do
     expect(subject.balance).to eq 0
   end
+  it 'registers a deposit' do
+    subject.deposit(20)
+    expect(subject.balance).to eq 20
+  end
 end
